155768e7ea424e3a7bfa1a7a1c6b312a8e968088
JUNGHUN KWON <kwonx149@umn.edu>
2013-09-16 15:24:23 -0500

    Junghun Kwon's Workday2

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,24 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullname As String
+        ' Read the input from the use
+        fullname = txtFullName.Text
+
+        'Trim the input so as to remove leading spaces
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        'Find the index of first space
+        Dim idxSpace As Integer
+        idxSpace = fullnameTrimmed.IndexOf(" ")
+
+        'Extract the first name
+        Dim firstName As String
+        firstName = fullnameTrimmed.Substring(0, idxSpace)
+
+        'Display the result to the user
+        txtFirstName.Text = firstName
 
     End Sub
 End Class
